d90f9d35dee9221c511fac7bd8092b87ea6b187b
Hitorina <grizzlizyping@gmail.com>
2023-02-21 21:45:27 +0700

    Latest Version

--- handler/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  token: 'MTA3NDE5NDMwNzg3Mjk5MzI4MA.G5Gp8l.o3NFIiqxtyhdOEzr3I7BkaN4Fmrst4ptW4Fhtc',
-  prefix: '-',
-  developer: [
-    "924088591787884594",
-    "1010474132753883207"
-  ],
-  serverId: '1066220891027013642'
-};
