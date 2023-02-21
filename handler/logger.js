9c8e929ef44038a16bf47da1d9f5f88bf8c687c9
Hitorina <grizzlizyping@gmail.com>
2023-02-21 19:05:17 +0700

    Latest Version

--- handler/logger.js
+++ handler/logger.js
@@ -1,3 +1,9 @@
+const client = require('../index');
+const { Events } = require('discord.js');
 const pino = require('pino');
 const logger = pino();
-module.exports = logger;
+
+client.on(Events.Debug, m => logger.debug(m));
+client.on(Events.Debug, m => logger.debug(m));
+client.on(Events.Warn, m => logger.warn(m));
+client.on(Events.Error, m => logger.error(m));
